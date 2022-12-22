99f7f1b6336a5865188129c629a95588a75af07b
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2022-12-22 11:12:22 +0900

    Add files via upload

---
+++ 4-1-3-1 even_odd.php
@@ -0,0 +1,22 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 1 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $num = 80;
+
+            if(($num % 2) == 0)
+            {
+                echo "$num : 짝수";
+            }
+            else
+            {
+                echo "$num : 홀수";
+            }
+        ?>
+    </body>
+</html>
