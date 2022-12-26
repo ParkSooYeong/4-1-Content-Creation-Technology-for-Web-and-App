51fe91e51a9610c39ef6e5616a88fd8e7003fef0
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2022-12-26 10:11:59 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-2 fee.php
@@ -0,0 +1,21 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 2 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $age = 25;
+            $fee = "1,500원";
+
+            if($age >= 13)
+            {
+                $fee = "2,000원";
+            }
+
+            echo "나이 : $age 세, 지하철 요금 : $fee";
+        ?>
+    </body>
+</html>
