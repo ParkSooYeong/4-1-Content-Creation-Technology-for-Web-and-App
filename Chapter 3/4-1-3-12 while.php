039366122044b850d2f1ffdf14c45521003903ec
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-09 10:08:40 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-12 while.php
@@ -0,0 +1,20 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 12 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $i = 1;            // 초기값 설정
+
+            while($i <= 10)    // 조건식 $i <= 10 이 참일 동안 반복
+            {
+                echo "$i<br>"; // $i를 출력하고 행 바꿈
+                
+                $i++;          // $i 값 1 증가
+            }
+        ?>
+    </body>
+</html>
