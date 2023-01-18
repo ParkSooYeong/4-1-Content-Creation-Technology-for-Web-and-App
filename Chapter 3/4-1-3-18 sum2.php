a6500e8f2261e8f34ec1ebf5b1ba3f73a787b159
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-18 14:47:39 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-18 sum2.php
@@ -0,0 +1,20 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 18 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $sum = 0;
+
+            for($a = 1; $a <= 10; $a++)
+            {
+                $sum = $sum + $a;
+            }
+
+            echo("1에서 10까지 자연수의 합은 $sum 입니다.<br>");
+        ?>
+    </body>
+</html>
