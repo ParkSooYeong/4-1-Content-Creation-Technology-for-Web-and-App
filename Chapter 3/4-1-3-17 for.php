8ffd32f56807be771d5f98cf3a374a509d5ab0a1
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-18 14:43:32 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-17 for.php
@@ -0,0 +1,25 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 17 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            for($i = 1; $i <= 10; $i++)
+            {
+                echo "$i<br>"; // $i를 화면에 출력하고 행 바꿈
+            }
+
+            $i = 1;
+
+            while($i <= 10)
+            {
+                echo "$i<br>";
+
+                $i++;
+            }
+        ?>
+    </body>
+</html>
