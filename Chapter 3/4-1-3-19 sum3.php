ffed1cd3dd9cb6b68b6599c9b3378aa662d8d84b
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-19 14:55:20 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-19 sum3.php
@@ -0,0 +1,20 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 19 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $sum = 0;
+
+            for($i = 1; $i <= 100; $i++)
+            {
+                $sum = $sum + $i;
+            }
+
+            echo "1과 100 사이에 있는 5의 배수의 합계 : $sum";
+        ?>
+    </body>
+</html>
