bcaed2f7c5cc9ba723935d273189484c717c5ddf
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-30 11:25:55 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-23 multiple2.php
@@ -0,0 +1,25 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 23 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            echo "------------------------------<br>";
+
+            for($a = 2; $a <= 9; $a++)
+            {
+                for($b = 1; $b <= 9; $b++)
+                {
+                    $c = $a * $b;
+
+                    echo "$a x $b = $c <br>";
+                }
+
+                echo "------------------------------<br>";
+            }
+        ?>
+    </body>
+</html>
