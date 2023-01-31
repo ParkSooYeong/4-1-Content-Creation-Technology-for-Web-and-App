0d1b30e89949d961fd70c200171ddc2035918785
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-01-31 12:43:22 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-24 star.php
@@ -0,0 +1,21 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 25 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            for($a = 1; $a <= 10; $a++)
+            {
+                for($b = 1; $b <= $a; $b++)
+                {
+                    echo "* ";
+                }
+
+                echo "<br>";
+            }
+        ?>
+    </body>
+</html>
