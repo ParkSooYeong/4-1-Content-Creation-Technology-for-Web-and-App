7e6af9cabdb8b824614c158c9b8aa92ff8613f46
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-02-07 20:03:21 +0900

    Add files via upload

---
+++ Chapter 3/4-1-3-26 do_while.php
@@ -0,0 +1,17 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 3 , Number 26 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $i = 100;
+
+            do {
+                echo $i."<br>";
+            } while ($i <= 10)
+        ?>
+    </body>
+</html>
