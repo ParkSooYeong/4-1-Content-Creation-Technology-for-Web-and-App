b5368ab846a2f407d3be054f691c1f6414c98224
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-02-15 23:09:05 +0900

    Add files via upload

---
+++ Chapter 4/4-1-4-5 func.php
@@ -0,0 +1,18 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 4 , Number 5 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            function func()
+            {
+                echo (" Hello World! ");
+            }
+
+            func();
+        ?>
+    </body>
+</html>
