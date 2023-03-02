cbe26c5321d63c5d37c87a936a96e30b4561c881
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-03-02 23:27:17 +0900

    Add files via upload

---
+++ Chapter 4/4-1-4-7 func_plus.php
@@ -0,0 +1,24 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 4 , Number 7 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            function plus($a, $b)
+            {
+                $c = $a + $b;
+
+                echo $c;
+            }
+
+            plus(15, 25);
+
+            echo "<br>";
+
+            plus(3500, 1500);
+        ?>
+    </body>
+</html>
