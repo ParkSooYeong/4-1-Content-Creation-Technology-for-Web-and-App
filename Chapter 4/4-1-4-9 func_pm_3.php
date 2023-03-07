32c330990498158fa8e1fe9dc33046577e0b805d
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-03-07 22:25:35 +0900

    Add files via upload

---
+++ Chapter 4/4-1-4-9 func_pm_3.php
@@ -0,0 +1,26 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 4 , Number 9 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            function pm($a, $b, $c)
+            {
+                $d = $a + $b - $c;
+
+                return $d;
+            }
+
+            $i = 500;
+            $j = 200;
+            $k = 300;
+
+            $result = pm($i, $j, $k);
+
+            echo $result;
+        ?>
+    </body>
+</html>
