7f4d4e62c539a4117a2153ff9355799da6fcb148
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-04-24 22:40:58 +0900

    Add files via upload

---
+++ Chapter 6/4-1-6-2 table.sql
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 4 , Semester 1 , Chapter 6 , Number 2 */
+
+create table test2 (
+    num int not null,
+    name char(10),
+    address char(80),
+    tel char(20),
+    primary key(num)
+);
