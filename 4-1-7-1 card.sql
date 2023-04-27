719b9a8ca321a7d140326291ddf3d1b10b3e7a8a
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-04-27 12:49:40 +0900

    Add files via upload

---
+++ 4-1-7-1 card.sql
@@ -0,0 +1,12 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 4 , Semester 1 , Chapter 7 , Number 1 */
+
+create table card (
+    num int,
+    name char(10),
+    company char(30),
+    tel char(20),
+    hp char(20),
+    address char(100),
+    primary key(num)
+);
