fdeba0dc0386685e3d25151b382e156ec650071f
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-04-30 22:13:25 +0900

    Add files via upload

---
+++ Chapter 7/4-1-7-4 mem_print.php
@@ -0,0 +1,25 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 7 , Number 4 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            echo "아이디 : $id<br>";
+            echo "이름 : $name<br>";
+            echo "패스워드 : $passwd<br>";
+            echo "패스워드 확인 : $passwd_confirm<br>";
+            echo "성별 : $gender<br>";
+            echo "휴대전화 번호 : $phone1 - $phone2 - $phone3<br>";
+            echo "주소 : $address<br>";
+            echo "영화 : $movie<br>";
+            echo "독서 : $book<br>";
+            echo "쇼핑 : $shop<br>";
+            echo "운동 : $sport<br>";
+            echo "자기소개 : $intro<br>";
+            echo "제목(hidden 타입에서 전달) : $title<br>";
+        ?>
+    </body>
+</html>
