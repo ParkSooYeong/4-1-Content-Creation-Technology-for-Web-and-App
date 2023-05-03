b7afe9e1b0f678487cf4c47d30f56777d9fa13a5
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-05-03 22:45:20 +0900

    Add files via upload

---
+++ Chapter 7/4-1-7-7 mysql_result.php
@@ -0,0 +1,55 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 7 , Number 7 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <?
+            $connect = mysql_connect("localhost", "abc", "1234");
+            $db_con = mysql_select_db("test_db", $connect);
+
+            $sql = "select * from membership where address like '%서울%' order by age;";
+            $result = mysql_query($sql, $connect);
+
+            $fields = mysql_num_fields($result);
+
+            $number = 1;
+        ?>
+
+        <h2>▶ mysql_result()를 이용한 데이터 읽기</h2>
+        <table width="800" border="1" cellspacing="0" cellpadding="5">
+            <tr align="center">
+                <td bgcolor="#cccccc">일련번호</td>
+                <td bgcolor="#cccccc">아이디</td>
+                <td bgcolor="#cccccc">이름</td>
+                <td bgcolor="#cccccc">우편번호</td>
+                <td bgcolor="#cccccc">주소</td>
+                <td bgcolor="#cccccc">전화번호</td>
+                <td bgcolor="#cccccc">나이</td>
+            </tr>
+
+            <?
+                for($i = 0; $i < $records; $i++)
+                {
+                    echo "<tr>";
+                    echo "<td>$number</td>";
+
+                    for($j = 0; $j < $fields; $j++)
+                    {
+                        $data = mysql_result($result, $i, $j);
+
+                        echo "<td>$data</td>";
+                    }
+
+                    echo "</tr>";
+
+                    $number++;
+                }
+
+                mysql_close();
+            ?>
+        </table>
+    </body>
+</html>
