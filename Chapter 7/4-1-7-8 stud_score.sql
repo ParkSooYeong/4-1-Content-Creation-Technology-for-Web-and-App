5f159fffecf53b26953e20bebc6944e0727fad06
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-05-04 23:55:22 +0900

    Add files via upload

---
+++ Chapter 7/4-1-7-8 stud_score.sql
@@ -0,0 +1,15 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 4 , Semester 1 , Chapter 7 , Number 8 */
+
+create table stud_score (
+    num int not null auto_increment,
+	name varchar(12),
+	sub1 int,
+	sub2 int,
+	sub3 int,
+	sub4 int,
+	sub5 int,
+	sum int,
+	avg float,
+	primary key(num)
+);
