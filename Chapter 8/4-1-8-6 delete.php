aa48dd8dbda230836288c97f4c8b2ad944c130af
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-06-07 12:06:16 +0900

    Add files via upload

---
+++ Chapter 8/4-1-8-6 delete.php
@@ -0,0 +1,16 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 8 , Number 6 -->
+
+<html>
+    <head>
+        <title>Memo Delete</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+    </head>
+    <body>
+        <form action=delete_ok.php method=post>
+            Password : <input type=password name='pass' size=8 maxlength=20> :
+            <input type=hidden name='no' value=<?=$no?>>
+            <input type=submit value='o.k'>
+        </form>
+    </body>
+</html>
