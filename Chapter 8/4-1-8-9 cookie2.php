ad08bf34edad5967e84221e8538d0aac81ef7525
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-06-10 01:20:13 +0900

    Add files via upload

---
+++ Chapter 8/4-1-8-9 cookie2.php
@@ -0,0 +1,14 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 8 , Number 9 -->
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+    </head>
+    <body>
+        <?
+            echo "쿠키 'userid' : $userid <br>";
+            echo "쿠키 'username' : $username <br>";
+        ?>
+    </body>
+</html>
