643dfd35d945ce6c8b8e595acde7d1cc8452c868
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-06-20 01:07:08 +0900

    Add files via upload

---
+++ Chapter 9/4-1-9-3 top_menu1.php
@@ -0,0 +1,38 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 9 , Number 3 -->
+
+<div class="menus">
+    <a href="./memo/memo.php">
+        <img src="./img/menu01.gif" border="0">
+    </a>
+</div> <!-- 낙서장 -->
+<div class="menus">
+    <a href="./greet/list.php">
+        <img src="./img/menu02.gif" border="0">
+    </a>
+</div> <!-- 가입인사 -->
+<div class="menus">
+    <a href="./concert/list.php">
+        <img src="./img/menu03.gif" border="0">
+    </a>
+</div> <!-- 소개 -->
+<div class="menus">
+    <a href="./download/list.php">
+        <img src="./img/menu04.gif" border="0">
+    </a>
+</div> <!-- 자료실 -->
+<div class="menus">
+    <a href="./free/list.php">
+        <img src="./img/menu05.gif" border="0">
+    </a>
+</div> <!-- 자유게시판 -->
+<div class="menus">
+    <a href="./qna/list.php">
+        <img src="./img/menu06.gif" border="0">
+    </a>
+</div> <!-- 레슨 문의 -->
+<div class="menus">
+    <a href="#">
+        <img src="./img/menu07.gif" onclick="window.open('./survey/survey.php', '', 'scrollbars=no, toolbars=no, width=180, height=230')" border="0">
+    </a>
+</div> <!-- 설문조사 -->
