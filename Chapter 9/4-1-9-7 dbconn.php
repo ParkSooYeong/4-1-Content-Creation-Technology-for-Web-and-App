81ed216275d593ed4cb949174ded1fa9aaf12957
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-06-24 02:00:10 +0900

    Add files via upload

---
+++ Chapter 9/4-1-9-7 dbconn.php
@@ -0,0 +1,8 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 9 , Number 7 -->
+
+<?
+    $connect = mysql_connect("localhost", "nzin", "1234") or die("SQL server에 연결할 수 없습니다.");
+
+    mysql_select_db("test_db", $connect);
+?>
