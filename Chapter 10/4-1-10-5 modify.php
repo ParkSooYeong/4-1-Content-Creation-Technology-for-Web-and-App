d334d922cf94ed0ea899f1f1218e5629ae8d3386
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-07-05 01:42:51 +0900

    Add files via upload

---
+++ Chapter 10/4-1-10-5 modify.php
@@ -0,0 +1,29 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 10 , Number 5 -->
+
+<?
+    session_start();
+?>
+
+<meta charset="euc-kr">
+
+<?
+    $hp = $hp1."-".$hp2."-".$hp3;
+    $email = $email1."@".$email2;
+    $regist_day = date("Y-m-d (H:i)"); // 현재 날짜(년-월-일-시-분) 저장
+
+    include "../lib/dbconn.php";       // dbconn.php 파일 호출
+
+    $sql = "update member set pass = '$pass', name = '$name', ";
+    $sql .= "nick = '$nick', hp = '$hp', email = '$email', regist_day = '$regist_day' where id = '$userid'";
+
+    mysql_query($sql, $connect);       // $sql에 저장된 명령 실행
+
+    mysql_close(); // 데이터베이스 연결 종료
+
+    echo ("
+        <script>
+            location.href='../index.php';
+        </script>
+    ");
+?>
