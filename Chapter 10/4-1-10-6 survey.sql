8eb812a63f0387f1ccabc84ed1e1a90c5b33cd72
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-07-06 01:43:17 +0900

    Add files via upload

---
+++ Chapter 10/4-1-10-6 survey.sql
@@ -0,0 +1,11 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 4 , Semester 1 , Chapter 10 , Number 6 */
+
+create table survey(
+    ans1 int,
+    ans2 int,
+    ans3 int,
+    ans4 int
+);
+
+insert into survey values(0, 0, 0, 0);
