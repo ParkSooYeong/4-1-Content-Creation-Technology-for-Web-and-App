7c75e4396dd00f128f364e3d90d3e0dd8d368d75
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2023-07-19 00:48:46 +0900

    Add files via upload

---
+++ Chapter 11/4-1-11-7 delete_ripple.php
@@ -0,0 +1,17 @@
+<!-- SKU CoE ITE - ParkSooYoung -->
+<!-- Grade 4 , Semester 1 , Chapter 11 , Number 7 -->
+
+<?
+    include "../lib/dbconn.php";
+    
+    $sql = "delete from memo_ripple where num = $num";
+    mysql_query($sql, $connect);
+    
+    mysql_close();
+    
+    echo ("
+        <script>
+            location.href = 'memo.php';
+        </script>
+    ");
+?>
